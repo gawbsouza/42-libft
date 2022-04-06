24416acb3825f6fcf1a88c16c71138e020d086d3
gasouza <gabril.strakh@gmail.com>
2022-04-06 17:25:40 +0200

    libft.h: adicionar protótipo ft_isalnum

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:01:14 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/06 17:00:42 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/06 17:14:22 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
 #endif
