eec3ae41764af343d8efd1c0ec5ea0ace11e5f10
gasouza <gabril.strakh@gmail.com>
2022-04-13 21:48:09 +0200

    ft_memset.c: criar arquivo

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 21:09:01 by gasouza           #+#    #+#             */
+/*   Updated: 2022/04/13 21:22:58 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < n)
+	{
+		*((unsigned char *) s + index) = (unsigned char) c;
+		index++;
+	}
+	return (s);
+}
