cfaf58c10424465823f6afa73c2bf101b9e5411a
gasouza <gabril.strakh@gmail.com>
2022-04-15 00:21:50 +0200

    ft_calloc.c: inicializar bytes com 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 19:24:06 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/14 20:35:21 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/15 00:17:14 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,16 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {	
+	void	*ptr;
+	size_t	ptr_size;
+
 	if (!size || !nmemb)
 		return (NULL);
 	if (nmemb > INT_MAX / size)
 		return (NULL);
-	return (malloc(nmemb * size));
+	ptr_size = nmemb * size;
+	ptr = malloc(ptr_size);
+	if (ptr != NULL)
+		ft_bzero(ptr, ptr_size);
+	return (ptr);
 }
