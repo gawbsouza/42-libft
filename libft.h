f1c6764f72a5562c6cd8eb7ede7c52fd5c595c95
gabriel <gabriel.souza@zoho.com>
2022-04-16 10:49:12 -0300

    libft.h: ajustar protótipo ft_memmove

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:01:14 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/15 00:26:40 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/16 10:38:50 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t c);
 void	*ft_memcpy(void *s1, const void *s2, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
