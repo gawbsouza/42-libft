db080f146b53e09e09c5622078bbe19c7b8671c8
gabriel <gabriel.souza@zoho.com>
2022-04-18 16:14:50 -0300

    libft.h: adicionar protótipo ft_strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:01:14 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/16 10:38:50 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/18 16:13:16 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
