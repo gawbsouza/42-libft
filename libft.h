f5dd4b08bac0e34b352161190c90ed395de2a677
gabriel <gabriel.souza@zoho.com>
2022-04-20 11:13:46 -0300

    libft.h: adicionar protótipo ft_strtrim

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:01:14 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/19 11:28:49 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/20 08:26:38 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *s);
+char	*ft_strtrim(const char *s1, const char *set);
 
 #endif
