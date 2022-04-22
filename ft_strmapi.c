d020dbd4fe7738401f1f91cf06c6d28797a919c4
gabriel <gabriel.souza@zoho.com>
2022-04-22 15:18:58 -0300

    ft_strmapi.c: criar arquivo

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 15:11:10 by gasouza           #+#    #+#             */
+/*   Updated: 2022/04/22 15:17:13 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	str = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
