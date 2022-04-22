326c7bebb825bf771556731d1032aa39262aebb7
gabriel <gabriel.souza@zoho.com>
2022-04-22 14:55:04 -0300

    libft.h: adicionar protótipo ft_itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 14:01:14 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/20 15:12:15 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/21 18:26:47 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strtrim(const char *s1, const char *set);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	**ft_split(const char *s, char c);
+char	*ft_itoa(int n);
 
 #endif
