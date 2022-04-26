60a4907d1263ae52d346d4ee2e0d9b67d62fa007
gabriel <gabriel.souza@zoho.com>
2022-04-26 17:59:47 -0300

    refatoração: reduzir utilização de variáveis

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:51:04 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/15 22:44:06 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/26 17:56:25 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t c)
 {
-	size_t			index;
-	unsigned char	char_s1;
-	unsigned char	char_s2;
+	size_t	i;
 
-	index = 0;
-	while (index < c)
+	i = 0;
+	while (i < c)
 	{
-		char_s1 = s1[index];
-		char_s2 = s2[index];
-		if (!char_s1 || !char_s2)
-			return (char_s1 - char_s2);
-		if (char_s1 != char_s2)
-			return (char_s1 - char_s2);
-		index++;
+		if (!s1[i] || !s2[i] || s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
 	}
 	return (0);
 }
