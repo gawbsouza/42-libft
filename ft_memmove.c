0bc550b8698c0a2b148a9dd0cb78a194d5ba818e
gabriel <gabriel.souza@zoho.com>
2022-04-26 22:58:00 -0300

    refatoração: utilizar somente ponteiros

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 21:07:56 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/20 11:20:15 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/26 22:56:58 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	size_t	index;
-	char	*buffer;
-	char	*tmp_dst;
-	char	*tmp_src;
+	void	*tmp;
 
-	if (n != 0)
+	tmp = dst;
+	while (n-- > 0)
 	{
-		tmp_dst = (char *) dst;
-		tmp_src = (char *) src;
-		buffer = ft_calloc(n, sizeof(char));
-		ft_memcpy(buffer, tmp_src, n);
-		index = n;
-		while (index > 0)
-		{
-			index--;
-			tmp_dst[index] = buffer[index];
-		}
-		free(buffer);
+		if (dst < src)
+			*(char *) tmp++ = *(char *) src++;
+		else
+			*(char *)(dst + n) = *(char *)(src + n);
 	}
 	return (dst);
 }
