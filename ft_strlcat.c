abff4e0287ef85e420e59007db9cdca9a05e0ae3
gabriel <gabriel.souza@zoho.com>
2022-04-27 10:52:11 -0300

    refatoração: utilizar somente ponteiros

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 00:33:40 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/18 14:42:35 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/27 10:49:42 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,13 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	cpy_len;
-	size_t	index;
-	size_t	final_len;
+	size_t	len;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	cpy_len = src_len + dst_len;
-	final_len = dst_len + 1;
-	if (dst_len > size)
-		return (src_len + size);
-	if (size <= final_len)
-		return (cpy_len);
-	index = 0;
-	while (src[index] && final_len + index < size)
+	len = ft_strlen(dst);
+	if (size > len)
 	{
-		dst[dst_len + index] = src[index];
-		index++;
+		ft_strlcpy(dst + len, src, size - len);
+		return (ft_strlen(src) + len);
 	}
-	dst[dst_len + index] = '\0';
-	return (cpy_len);
+	return (ft_strlen(src) + size);
 }
