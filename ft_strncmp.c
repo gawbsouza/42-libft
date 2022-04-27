a6b930da0e858d42606735a3c67d4d841b3a7819
gabriel <gabriel.souza@zoho.com>
2022-04-26 22:17:15 -0300

    refatoração: remover variáveis auxiliares

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 16:51:04 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/26 17:56:25 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/26 22:13:21 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t c)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < c)
+	while (c-- > 0)
 	{
-		if (!s1[i] || !s2[i] || s1[i] != s2[i])
-			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
-		i++;
+		if (!*s1 || !*s2 || *s1 != *s2)
+			return ((unsigned char) *s1 - (unsigned char) *s2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
