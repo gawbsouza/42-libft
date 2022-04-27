a06b6c252d7520a072106197cbc467537be94b76
gabriel <gabriel.souza@zoho.com>
2022-04-26 22:10:14 -0300

    refatoração: utilizar somente ponteiros

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 21:55:28 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/13 22:41:39 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/26 22:08:48 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			index;
-	unsigned char	val1;
-	unsigned char	val2;
-
-	index = 0;
-	while (index < n)
+	while (n-- > 0)
 	{
-		val1 = *((unsigned char *) s1 + index);
-		val2 = *((unsigned char *) s2 + index);
-		if (val1 != val2)
-			return (val1 - val2);
-		index++;
+		if (!s1 || !s2 || *(unsigned char *) s1 != *(unsigned char *) s2)
+			return (*(unsigned char *) s1 - *(unsigned char *) s2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
