c5df04876e3f488a25152a75548f49caea598671
gabriel <gabriel.souza@zoho.com>
2022-04-28 11:50:08 -0300

    ft_itoa.c: calcular tamanho string automaticamente

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,14 +6,14 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 18:27:41 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/22 14:36:59 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/28 11:49:00 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static char	*itoa_rec(int n, char *str)
+static char	*calc(int n, char *str)
 {
 	char	digit;
 
@@ -30,19 +30,39 @@ static char	*itoa_rec(int n, char *str)
 			n *= -1;
 		}
 		if (n > 0)
-			str = itoa_rec(n, str);
+			str = calc(n, str);
 		*str = digit + '0';
 	}
 	return (str + 1);
 }
 
+static size_t	num_len(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
+	size_t	len;
 
-	str = malloc(12 * sizeof(char));
+	if (n == 0)
+		return (ft_strdup("0"));
+	len = num_len(n);
+	str = malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	*(itoa_rec(n, str)) = '\0';
+	calc(n, str);
+	str[len] = '\0';
 	return (str);
 }
