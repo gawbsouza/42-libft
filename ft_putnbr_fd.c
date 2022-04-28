7bc0b3874a596b64bf223851e37de623b1c2b075
gabriel <gabriel.souza@zoho.com>
2022-04-28 12:22:52 -0300

    ft_putnbr_fd.c: remover ft_itoa

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 15:49:31 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/22 15:52:34 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/28 12:19:48 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char	digit;
+
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+		return ;
+	}
+	digit = (n % 10);
+	n /= 10;
+	if (n < 0 || digit < 0)
+	{
+		ft_putchar_fd('-', fd);
+		digit *= -1;
+		n *= -1;
+	}
+	if (n > 0)
+		ft_putnbr_fd(n, fd);
+	ft_putchar_fd(digit + '0', fd);
 }
