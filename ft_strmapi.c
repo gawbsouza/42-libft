a0c2e1a6d4354a7b8c53e0891d30af5e231d451e
gabriel <gabriel.souza@zoho.com>
2022-04-28 11:59:46 -0300

    ft_strmapi.c: simplificar iteração

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 15:11:10 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/22 15:17:13 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/28 11:58:37 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 	char	*str;
 	size_t	i;
 
-	str = malloc((ft_strlen(s) + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s[i])
+	str = ft_strdup(s);
+	if (str != NULL)
 	{
-		str[i] = f(i, s[i]);
-		i++;
+		i = 0;
+		while (s[i])
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
 	}
-	str[i] = '\0';
 	return (str);
 }
