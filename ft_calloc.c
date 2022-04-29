5a4710c82e1b159870db2205c2dfacb8eae6b67a
gabriel <gabriel.souza@zoho.com>
2022-04-28 22:48:30 -0300

    ft_calloc.c: implementar limite de int localmente

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,22 +6,30 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 19:24:06 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/15 00:17:14 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/28 22:47:23 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
-#include <limits.h>
+
+static int	power(int num, int pow)
+{
+	if (pow <= 1)
+		return (num);
+	return (num * power(num, pow - 1));
+}
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {	
 	void	*ptr;
 	size_t	ptr_size;
+	int		max_int;
 
 	if (!size || !nmemb)
 		return (NULL);
-	if (nmemb > INT_MAX / size)
+	max_int = power(2, (sizeof(int) * 8) - 1) - 1;
+	if (nmemb > max_int / size)
 		return (NULL);
 	ptr_size = nmemb * size;
 	ptr = malloc(ptr_size);
