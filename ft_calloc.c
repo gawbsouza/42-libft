cf2bce6080a685d7ad60824fe0ec04bc3d8e9629
gabriel <gabriel.souza@zoho.com>
2022-04-28 23:08:00 -0300

    ft_calloc.c: simplificar max size

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,30 +6,23 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 19:24:06 by gasouza           #+#    #+#             */
-/*   Updated: 2022/04/28 22:47:23 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/28 23:07:09 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	power(int num, int pow)
-{
-	if (pow <= 1)
-		return (num);
-	return (num * power(num, pow - 1));
-}
-
 void	*ft_calloc(size_t nmemb, size_t size)
 {	
 	void	*ptr;
 	size_t	ptr_size;
-	int		max_int;
+	size_t	max_size;
 
 	if (!size || !nmemb)
 		return (NULL);
-	max_int = power(2, (sizeof(int) * 8) - 1) - 1;
-	if (nmemb > max_int / size)
+	max_size = -1;
+	if (nmemb > max_size / size)
 		return (NULL);
 	ptr_size = nmemb * size;
 	ptr = malloc(ptr_size);
